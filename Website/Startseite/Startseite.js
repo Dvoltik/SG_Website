26d0da3f5311f63982198eaec0eb6a7c4aef64ae
Dvoltik <dominik.wolters@sinsheim-ost.de>
2024-08-17 19:25:15 +0200

    News vollständig eingeführt
    News teil Front end komplett fertig, backend (Daten aus Jason) noch nicht angefangen

---
+++ Website/Startseite/Startseite.js
@@ -0,0 +1,87 @@
+onload = () => {
+    startTimer();
+}
+
+
+let n;
+let i = 1;
+let timeNextSlideS = 10;                        //Sekunden nach denen die nächste Newsslide kommt
+let timeNextSlideMS = timeNextSlideS * 1000;    //Millisekunden nachdenen die nächste Newsslide kommt
+
+let timerNextSlide;
+
+let white = 0;
+
+let slides = document.getElementById('.News' + i);
+
+
+function hoverNews(){           
+    pauseTimer();
+}
+
+
+
+function exitNews(){
+    startTimer();
+}
+
+
+
+function startTimer(){
+    console.log("startTimer");
+    console.log(timeNextSlideMS);
+    timerNextSlide = setTimeout(nextSlide, timeNextSlideMS);
+    return timerNextSlide;
+}
+
+function pauseTimer(){
+    console.log("pauseTimer");
+    clearInterval(timerNextSlide);
+}
+
+
+function nextSlide(){
+    n = 1;
+    console.log("next");
+    console.log(slides);
+    plusSlides(n);
+    return n;
+    
+}
+
+function prevSlide(){
+    n = -1;
+    console.log("prev");
+    plusSlides(n);
+    //return n;
+    
+}
+
+function plusSlides(n){
+
+    checkSlide();
+    console.log(i);
+
+    slides = document.getElementById('News' + i);
+    slides.style.display = "none";
+
+    i = i + n;
+    checkSlide();
+    slides = document.getElementById('News' + i);
+    slides.style.display = "block";
+    
+   
+    startTimer();
+    return i;
+}
+
+function checkSlide(){
+    if(i < 1){
+        i = 4;
+    } else if (i > 4){
+        i = 1;
+    }
+    return i;
+}
+
+
