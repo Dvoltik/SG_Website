0e4f6010d1a5438d7631fea1b22f6e65daecd661
kraken tutorial <dvoltuek@gmail.com>
2024-09-14 17:34:45 +0200

    Automatisierung angefangen
    Für die Herren Seiten die automatisierung mit Arrays als datenbanken zugefügt (Trainingszeiten und Liga Tabelle).

---
+++ Website/Mannschaften/Herren1.js
@@ -0,0 +1,102 @@
+const h1 = [
+
+    {"mannschaft": "Herren1 (Oberliga)", "liga": "NVV Oberliga"},
+
+]
+
+const mannschaftsBild = [
+    {"bild1": 'url("")'}
+]
+
+const trainer = [
+    "Rudi Sonebichler", "Mail des Trainers"
+]
+
+const training = [
+    {"tag": "Montag", "zeit": "20:00 - 22:00 Uhr", "ort": '<a href="https://maps.app.goo.gl/vNNmGFdxrRNwishW7">Schwarzbachhalle Helmstadt-Bargen</a>'},
+    {"tag": "Mittwoch", "zeit": "20:00 - 22:00 Uhr", "ort": '<a href="https://maps.app.goo.gl/vNNmGFdxrRNwishW7">Schwarzbachhalle Helmstadt-Bargen</a>'},
+    {"tag": "Donnerstag", "zeit": "20:00 - 22:00 Uhr", "ort": '<a href="https://maps.app.goo.gl/vNNmGFdxrRNwishW7">Schwarzbachhalle Helmstadt-Bargen</a>'}
+]
+
+const tabelle = [
+    {"logo": "",    "name": "VSG Kleinsteinbach",       "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "2"},
+    {"logo": "",    "name": "VC Weil",                  "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "7"},
+    {"logo": "",    "name": "VC Offenburg",             "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "4"},
+    {"logo": "",    "name": "TV Kappelrodeck",          "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "4"},
+    {"logo": "",    "name": "TV Bühl 2",                "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "5"},
+    {"logo": "",    "name": "SG Sinsheim/Helmstadt",    "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "1"},
+    {"logo": "",    "name": "SG Heidelberg 3",          "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "6"},
+    {"logo": "",    "name": "SG Heidelberg 2",          "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "2"},
+    {"logo": "",    "name": "KIT Sport-Club 2010",      "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "3"},
+    {"logo": "",    "name": "FT 1844 Freiburg 3",       "spiele": "-", "siege": "-", "sätze": "-:-", "punkte": "2"},
+
+]
+
+const spieler = [
+    {"vorname":"Johannes", "nachname":"Dumke", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Mika", "nachname":"Dumke", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Michael", "nachname":"Rossel", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Jonas", "nachname":"Paar", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Tom", "nachname":"Dumke", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Dominik", "nachname":"Wolters", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Alex", "nachname":"Dumke", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Pascal", "nachname":"Dumke", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Tobias", "nachname":"Dumke", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Finnian", "nachname":"Dumke", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Bernd", "nachname":"Dumke", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Julian", "nachname":"Paar", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Benjamin", "nachname":"Himmel", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "},
+    {"vorname":"Luki", "nachname":"Dumke", "bild": "../Bilder/H1/JohannesDumke", "körpergröße": "185", "spielernummer": "5", "position": "Zuspieler", "warumVBall": " ", "warumSG": " ", "wieLangeSG": "", "tiktok": " ", "insta": " ", "facebook": " "}
+];
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    //Welche Mannschaft + Liga
+    const mannschaft = document.getElementById("mannschaft");
+    mannschaft.innerHTML = h1[0].mannschaft;
+
+
+    //Trainingszeiten + trainer
+    const trainername = document.getElementById("trainername");
+    const trainerKontakt = document.getElementById("trainerKontakt");
+    
+    trainername.innerHTML = trainer[0];
+    trainerKontakt.innerHTML = trainer[1];
+
+    for (var i = 0; i < training.length; i++){
+        let count = i + 1;
+        let trainningTag = document.getElementById("trainingTag" + count);
+        let trainingZeit = document.getElementById("trainingZeit" + count);
+        let trainingOrt = document.getElementById("trainingOrt" + count);
+
+        
+        trainningTag.innerHTML = training[i].tag;
+        trainingZeit.innerHTML = training[i].zeit;
+        trainingOrt.innerHTML = training[i].ort;
+    }
+
+    tabelle.sort((a, b) => {
+        return b.punkte - a.punkte;
+    });
+
+    console.log(tabelle);
+
+    for (var i = 0; i < tabelle.length; i++){
+        let count = i + 1;
+        
+        let logo = document.getElementById("logo" + count);
+        let team = document.getElementById("team" + count);
+        let spiele = document.getElementById("spiele" + count);
+        let siege = document.getElementById("siege" + count);
+        let sätze = document.getElementById("sätze" + count);
+        let punkte = document.getElementById("punkte" + count);
+
+        logo.innerHTML = tabelle[i].logo;
+        team.innerHTML = tabelle[i].name;
+        spiele.innerHTML = tabelle[i].spiele;
+        siege.innerHTML = tabelle[i].siege;
+        sätze.innerHTML = tabelle[i].sätze;
+        punkte.innerHTML = tabelle[i].punkte;
+    }
+});
